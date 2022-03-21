85430ae7dc843e46de770c19d7134222c7e57dc2
Smesaz <semesile@gmail.com>
2022-03-21 11:31:02 -0500

    fronted updated 3

--- triquigame/src/App.js
+++ triquigame/src/App.js
@@ -9,82 +9,118 @@ import {
 } from "./TriquiValidators";
 
 function App() {
-
-  const [player, setPlayer] = useState({
-    before:"O",
-    after:"X"
+  
+  const [state, setState] = useState({
+    player: "X",
+    counter: 0,
   });
-
-  const [grid, setGrid] = useState([Array(3).fill("-"), Array(3).fill("-"), Array(3).fill("-")]);
+  const [grid, setGrid] = useState([Array(3).fill(""), Array(3).fill(""), Array(3).fill("")]);
   
   const HandlePlayerChange1 = (e) => {
     e.preventDefault();
     let position = e.target.id.split("-")
-    console.log("row", position[0])
-    console.log("colum", position[1])
-    grid[position[0]][position[1]]=player.after;
-    isWinner(position[0], position[1], grid, player.after)
-    setPlayer({
-      ...player,
-      before: "X",
-      after:"O",
-    });
+    grid[position[0]][position[1]] = state.player;
+    triquiLogic(e, position[0], position[1], grid, state.player, "O")
   }
 
   const HandlePlayerChange2 = (e) => {
     e.preventDefault();
     let position = e.target.id.split("-")
+    grid[position[0]][position[1]] = state.player;
+    triquiLogic(e, position[0], position[1], grid, state.player, "X")
+  }
 
-    grid[position[0]][position[1]]=player.after;
-    isWinner(position[0], position[1], grid, player.after)
-    setPlayer({
-      ...player,
-      before:"O",
-      after:"X",
-    });
+  const triquiLogic = (e, row, col, grid, player, mark) => {
+    if (isWinner(row, col, grid, player)) {
+      HandleReset(e);
+    } else if (state.counter === 9) {
+      message();
+      HandleReset(e);
+    } else {
+      setState({
+        ...state,
+        player: mark,
+      });
+    }
   }
 
   const HandleReset = (e) => {
     e.preventDefault();
-    setGrid([Array(3).fill("-"), Array(3).fill("-"), Array(3).fill("-")]);
+    setGrid([Array(3).fill(""), Array(3).fill(""), Array(3).fill("")]);
+    setState({
+      ...state,
+      player: "X",
+      counter: 0,
+    });
+  }
+  
+  const isWinner = (row, col, grid, turn) => {
+    setState({
+      ...state,
+      counter: state.counter++,
+    })
+    if (validateRowWinner(row, grid, turn) || validateColumnWinner(col, grid, turn) || validateIdentical(grid, turn) || validateDiagonal(grid, turn)) {
+      message(turn);
+      return true
+    }
+    return false
   }
 
-  const isWinner = (row, col, grid, turn) => {
-
-    if(validateRowWinner(row, grid, turn) || validateColumnWinner(col, grid, turn) || validateIdentical(grid, turn) || validateDiagonal(grid, turn)) {
-      if(turn==="X"){
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: "Gana el jugador 1",
-          text: "Felicitaciones!",
-        });
-      } else {
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: "Gana el jugador 2",
-          text: "Felicitaciones!!!",
-        });
-      }
-      setGrid([Array(3).fill("-"), Array(3).fill("-"), Array(3).fill("-")]);
+  const message = (turn = "") => {
+    if (turn === "X"){
+      Swal.fire({
+        title: 'Felicidades JUGADOR 1 Ganaste!',
+        width: 600,
+        padding: '3em',
+        color: '#716add',
+        background: '#fff',
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("https://sweetalert2.github.io/images/nyan-cat.gif")
+          left top
+          no-repeat
+        `
+      });
+    } else if (turn === "O"){
+      Swal.fire({
+        title: 'Felicidades JUGADOR 2 Ganaste!',
+        width: 600,
+        padding: '3em',
+        color: '#716add',
+        background: '#fff',
+        backdrop: `
+          rgba(0,0,123,0.4)
+          url("https://sweetalert2.github.io/images/nyan-cat.gif")
+          left top
+          no-repeat
+        `
+      })
+    } else {
+      Swal.fire({
+        position: "center",
+        icon: "warning",
+        title: "Empate",
+        text: "No hay ganador!!!",
+      });
     }
-
   }
 
   return (
     <div className="App">
       <h1>Triqui Game</h1>
+      {state.player==="X"?
+      <h2 sty>Turno: Player 1 (X)</h2>:
+      <h2>Turno: Player 2 (O)</h2>}
       {grid.map((row, rowId) => {
 
         return(
-          <div>
+          <div className='row'>
             {row.map((cell, columnId) => {
           
               return(
-                cell!=="-"?
-                <button id={`${rowId}-${columnId}`}>{cell}</button> 
-                :player.after==="X" ? 
+                cell!==""?
+                <button id={`${rowId}-${columnId}`} className={cell} >{cell}</button> 
+                :state.player==="X" ? 
                 <button id={`${rowId}-${columnId}`}  onClick={HandlePlayerChange1}>{cell}</button> : 
                 <button id={`${rowId}-${columnId}`}  onClick={HandlePlayerChange2}>{cell}</button>) 
             })}
@@ -93,7 +129,7 @@ function App() {
       })}
       <div className='menu'>
         <button className='buttons' onClick={HandleReset}>Reset</button>
-        <button className='buttons' onClick={HandleReset}>Historial</button>
+        <button className='buttons' onClick={HandleReset}>Records</button>
       </div>
     </div>
   );
